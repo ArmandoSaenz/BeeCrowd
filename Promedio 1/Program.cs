3b84814a4eb0f31839f9a8aa5de5d8617dac143e
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2022-06-04 18:31:17 -0500

    Solución al problema de Promedio_1

---
+++ Promedio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2;
+            int n = 2;
+            n1 = float.Parse(Console.ReadLine());
+            n2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("{0}", (n1 + n2) / n);
+            Console.ReadLine();
+
+        }
+    }
+}
--- StarRating/Program.cs
+++ StarRating/Program.cs
@@ -8,8 +8,36 @@ namespace StarRating
 {
     internal class Program
     {
+        static string[] StarRating(string rating, int stars)
+        {
+            double rate;
+            rate = double.Parse(rating);
+            string[] images = new string[stars];
+            for (int i = 0; i < stars; i++)
+            {
+                if (rate > i + 1)
+                {
+                    images[i] = "full";
+                    continue;
+                }
+                if (rate > i && rate < i + 1)
+                {
+                    images[i] = "half";
+                    continue;
+                }
+                images[i] = "empty";
+            }
+            return images;
+        }
         static void Main(string[] args)
         {
+            string[] starrate;
+            string avg;
+            avg = Console.ReadLine();
+            starrate = StarRating(avg, 5);
+            Console.WriteLine(String.Join(" ", starrate));
+            Console.ReadLine();
+
         }
     }
 }
