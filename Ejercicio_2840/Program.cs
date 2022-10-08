f69ed9c10bdb21663cf137262e9f65fd5092db0a
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2022-10-08 01:16:16 -0500

    Ejercicio 2840

---
+++ Ejercicio_2840/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        string[] resultado = s.Split(' ');
+        int R = int.Parse(resultado[0]);
+        int L = int.Parse(resultado[1]);
+        float pi = 3.1415f;
+        double V = (4d / 3d) * (pi * R * R * R);
+        Console.WriteLine(L / (int)V);
+        Console.ReadLine();
+    }
+}
