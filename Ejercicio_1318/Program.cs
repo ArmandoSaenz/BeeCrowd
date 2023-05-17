5781f30912010f146925cbbbf4a0069a786fe369
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2023-05-16 18:17:36 -0600

    Compentecia Mayo 2023

---
+++ Ejercicio_1318/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+class URI
+{
+    static void Main()
+    {
+        int N, M;
+        List<string> tickets, originales, duplicados;
+        string line;
+        string[] data;
+        while (true)
+        {
+            line = Console.ReadLine();
+            if (line == "0 0")
+                break;
+            data = line.Split(' ');
+            N = int.Parse(data[0]);
+            M = int.Parse(data[1]);
+            line = Console.ReadLine();
+            tickets = line.Split(' ').ToList();
+            originales = tickets.Distinct().ToList();
+            duplicados = originales.FindAll(ticket => tickets.Count(t => t == ticket) > 1);
+            Console.WriteLine(duplicados.Count);
+        }
+    }
+}
---
+++ Ejercicio_1536/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Ejercicio_1536
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            string[] values;
+            int games;
+            int score1_l, score1_v, score2_l, score2_v;
+            int point1, point2;
+            data = Console.ReadLine();
+            games = int.Parse(data);
+            for (int i = 0; i < games; ++i)
+            {
+                data = Console.ReadLine();
+                values = data.Split('x');
+                score1_l = int.Parse(values[0].Trim());
+                score2_v = int.Parse(values[1].Trim());
+                data = Console.ReadLine();
+                values = data.Split('x');
+                score2_l = int.Parse(values[0].Trim());
+                score1_v = int.Parse(values[1].Trim());
+                point1 = 0;
+                point2 = 0;
+                //Puntos del primer juego
+                switch(Math.Sign(score1_l-score2_v))
+                {
+                    case 1:
+                        point1 += 3;
+                        break;
+                    case -1:
+                        point2 += 3;
+                        break;
+                    case 0:
+                        ++point1;
+                        ++point2;
+                        break;
+                }
+                //Puntos del segundo juego
+                switch (Math.Sign(score1_v - score2_l))
+                {
+                    case 1:
+                        point1 += 3;
+                        break;
+                    case -1:
+                        point2 += 3;
+                        break;
+                    case 0:
+                        ++point1;
+                        ++point2;
+                        break;
+                }
+                //Determinando ganador
+                switch(Math.Sign(point1-point2))
+                {
+                    case 1:
+                        Console.WriteLine("Time 1");
+                        break;
+                    case -1:
+                        Console.WriteLine("Time 2");
+                        break;
+                    case 0:
+                        switch(Math.Sign(score1_v-score2_v))
+                        {
+                            case 1:
+                                Console.WriteLine("Time 1");
+                                break;
+                            case -1:
+                                Console.WriteLine("Time 2");
+                                break;
+                            case 0:
+                                Console.WriteLine("Penaltis");
+                                break;
+                        }
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
