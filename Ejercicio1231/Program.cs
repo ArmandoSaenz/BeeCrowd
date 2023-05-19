a244c98d0b0f761b22ca17f9a1cdc009dfbe5c2e
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2023-05-18 20:40:23 -0600

    Ejercicio 3002

---
+++ Ejercicio1231/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1231
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            string[] values;
+            int n1, n2;
+            List<string> set1, set2;
+            while (true)
+            {
+                //Revisando EOF
+                if (String.IsNullOrEmpty((data = Console.ReadLine())))
+                {
+                    break;
+                }
+                //Obtiendo el tamaño de todos los dos conjuntos
+                values = data.Split(' ');
+                n1 = int.Parse(values[0]);
+                n2 = int.Parse(values[1]);
+                //Adquiriendo conjunto 1
+                set1 = new List<string>();
+                for(int i = 0; i < n1; ++i)
+                    set1.Add(Console.ReadLine());
+                //Adquiriendo conjunto 2
+                set2 = new List<string>();
+                for (int i = 0; i < n2; ++i)
+                    set2.Add(Console.ReadLine());
+                if(n2 == 0)
+                {
+                    Console.WriteLine("N");
+                    continue;
+                }
+                if(set1.TrueForAll(item => item.Contains('0') && !item.Contains('1')) && set2.TrueForAll(item => item.Contains('0') && !item.Contains('1')))
+                {
+                    Console.WriteLine("S");
+                    continue;
+                }
+                if (set1.TrueForAll(item => item.Contains('1') && !item.Contains('0')) && set2.TrueForAll(item => item.Contains('1') && !item.Contains('0')))
+                {
+                    Console.WriteLine("S");
+                    continue;
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_3002/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3002
+{
+    internal class Program
+    {
+        static bool IsPrime(int number)
+        {
+            if(number%2==0 && number > 2)
+                return false;
+            int max = (int)Math.Ceiling(Math.Sqrt(number));
+            if (max * max == number)
+                return false;
+            for (int i = 3; i < max; i += 2)
+                if (number % i == 0) return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            int number = int.Parse(data);
+            if (number % 2 == 0 && number > 2)
+                Console.WriteLine(2);
+            else if(IsPrime(number))
+                Console.WriteLine(1);
+            else if(IsPrime(number-2))
+                Console.WriteLine(2);
+            else
+                Console.WriteLine(3);
+            Console.ReadLine();
+        }
+    }
+}
