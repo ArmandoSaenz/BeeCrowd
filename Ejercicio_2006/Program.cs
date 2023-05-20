0a572d38535b41eb7de2b72038e594067a8e3cca
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2023-05-19 20:12:30 -0600

    Ejercicio 2006

---
+++ Ejercicio_2006/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            string[] values;
+            int tea;
+            int[] answers;
+            int output;
+            data = Console.ReadLine();
+            tea = int.Parse(data);
+            data = Console.ReadLine();
+            values = data.Split(' ');
+            answers = Array.ConvertAll<string, int>(values, int.Parse);
+            output = answers.Count(arg => arg == tea);
+            Console.WriteLine(output);
+        }
+    }
+}
