dade9a8a05e8bf2964c6b177d51657addb6296ea
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2023-05-24 11:31:53 -0600

    Ejercicio 1125 y 2549

---
+++ Ejercicio_1125/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1125
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            string[] values;
+            List<int>[] arrives;
+            List<int>[] scoring;
+            
+        }
+    }
+}
