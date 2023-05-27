4f4b1555953367e90d5a22d2b744fc82b48632ab
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2023-05-26 23:31:13 -0600

    2002

--- Ejercicio_2002/Program.cs
+++ Ejercicio_2002/Program.cs
@@ -1,53 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Ejercicio_2002
 {
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Equals(Point point) => X == point.X && Y == point.Y;
+        public bool Equals(int x, int y) => X == x && Y == y;
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    public class Djkistra
+    {
+        long Comp (KeyValuePair<Point, long> item) => item.Value;
+        public List<KeyValuePair<Point, long>> queue { get; set; } = new List<KeyValuePair<Point, long>>();
+        public bool Empty => queue.Count == 0;
+        public KeyValuePair<Point, long> Top() => queue.First();
+        public void Pop()
+        {
+            queue.RemoveAt(0);
+        }
+        public void Push(Point point, long cost)
+        {
+            if (Empty)
+            {
+                queue.Add(new KeyValuePair<Point, long>(point, cost));
+            }
+            else
+            {
+                if (queue[0].Value > cost)
+                    queue.Insert(0, new KeyValuePair<Point, long>(point, cost));
+                else
+                {
+                    int id = queue.FindIndex(item => item.Value >= cost);
+                    if(id == -1)
+                        queue.Add(new KeyValuePair<Point, long>(point, cost));
+                    else
+                        queue.Insert(id, new KeyValuePair<Point, long>(point, cost));
+                }
+
+            }   
+        }
+    }
     internal class Program
     {
-        static long[,] board;
-        static List<long> costs;
-        static List<int> next;
-        static (int, int) Id2Pos(int id, int ncols)
-        {
-            int i = id / ncols;
-            int j = id % ncols;
-            return (i, j);
-        }
-        static int Pos2Id(int i, int j, int ncols)
-        {
-            int id = i * ncols + j;
-            return id;
-        }
-        static (bool, long) MoveUp(int x, int y, int N)
-        {
-            if (x + 1 >= N)
-                return (false, 0);
-            return (true, C(board[x, y], board[x + 1, y]));
-        }
-        static (bool, long) MoveDown(int x, int y)
-        {
-            if (x - 1 < 0)
-                return (false, 0);
-            return (true, C(board[x, y], board[x - 1, y]));
-
-        }
-        static (bool, long) MoveLeft(int x, int y)
-        {
-            if (y - 1 < 0)
-                return (false, 0);
-            return (true, C(board[x, y], board[x, y - 1]));
-
-        }
-        static (bool, long) MoveRight(int x, int y, int M)
-        {
-            if (y + 1 >= M)
-                return (false, 0);
-            return (true, C(board[x, y], board[x, y + 1]));
-        }
         static long Complement(long x)
         {
             switch (x % 4)
@@ -70,11 +75,14 @@ namespace Ejercicio_2002
         }
         static void Main(string[] args)
         {
+            long[,] board;
+            bool[,] procesado;
+            Djkistra dijkstra;
+            KeyValuePair<Point, long> current;
             string data;
             string[] values;
-            int n, m, x, y, id, idnext, lastId;
+            int n, m, x, y;
             long cost;
-            bool valido;
             while (true)
             {
                 //Read first line
@@ -86,66 +94,43 @@ namespace Ejercicio_2002
                 m = int.Parse(values[1]);
                 //Create the board
                 board = new long[n, m];
-                costs = new List<long>(new long[n*m]);
-                lastId = n * m - 1;
-                next = new List<int>();
+                procesado = new bool[n, m];
                 //Get the cells cost
                 for (int i = 0; i < n; i++)
                 {
                     data = Console.ReadLine();
                     values = data.Split(' ');
                     for (int j = 0; j < m; j++)
-                        board[i, j] = long.Parse(values[j]);
-                }
-                next.Add(0);
-                //Dikjistra algorithm
-                while (next.Count > 0)
-                {
-                    id = next.First();
-                    (x, y) = Id2Pos(id, m);
                     {
-                        idnext = Pos2Id(x + 1, y, m);
-                        (valido, cost) = MoveUp(x, y, n);
-                        cost += costs[id];
-                        if (valido && idnext != 0 && (costs[idnext] == 0 || cost < costs[idnext]))
-                        {
-                            costs[idnext] = cost;
-                            if(!next.Contains(idnext))
-                                next.Add(idnext);
-                        }
-
-                        idnext = Pos2Id(x - 1, y, m);
-                        (valido, cost) = MoveDown(x, y);
-                        cost += costs[id];
-                        if (valido && idnext != 0 && (costs[idnext] == 0 || cost < costs[idnext]))
-                        {
-                            costs[idnext] = cost;
-                            if (!next.Contains(idnext))
-                                next.Add(idnext);
-                        }
-
-                        idnext = Pos2Id(x, y + 1, m);
-                        (valido, cost) = MoveRight(x, y, m);
-                        cost += costs[id];
-                        if (valido && idnext != 0 && (costs[idnext] == 0 || cost < costs[idnext]))
-                        {
-                            costs[idnext] = cost;
-                            if (!next.Contains(idnext))
-                                next.Add(idnext);
-                        }
-                        idnext = Pos2Id(x, y - 1, m);
-                        (valido, cost) = MoveLeft(x, y);
-                        cost += costs[id];
-                        if (valido && idnext != 0 && (costs[idnext] == 0 || cost < costs[idnext]))
-                        {
-                            costs[idnext] = cost;
-                            if (!next.Contains(idnext))
-                                next.Add(idnext);
-                        }
-                        next.RemoveAt(0);
+                        board[i, j] = long.Parse(values[j]);
                     }
                 }
-                Console.WriteLine("{0}", costs[lastId]);
+                dijkstra = new Djkistra();
+                dijkstra.Push(new Point(0, 0), 0);
+                //Dikjistra algorithm
+                while (!dijkstra.Empty)
+                {
+                    current = dijkstra.Top();
+                    dijkstra.Pop();
+                    (cost, x, y) = (current.Value, current.Key.X, current.Key.Y);
+                    if (procesado[x, y])
+                        continue;
+                    procesado[x, y] = true;
+                    if (x == n - 1 & y == m - 1)
+                    {
+                        Console.WriteLine(cost);
+                        break;
+                    }
+                    if (x - 1 >= 0 && !procesado[x - 1, y])
+                        dijkstra.Push(new Point(x - 1, y), cost + C(board[x - 1, y], board[x, y]));
+                    if (x +1 < n && !procesado[x + 1, y])
+                        dijkstra.Push(new Point(x + 1, y), cost + C(board[x + 1, y], board[x, y]));
+                    if (y - 1 >= 0 && !procesado[x, y - 1])
+                        dijkstra.Push(new Point(x, y - 1), cost + C(board[x, y - 1], board[x, y]));
+                    if (y + 1 < m && !procesado[x, y + 1])
+                        dijkstra.Push(new Point(x, y+1), cost + C(board[x, y + 1], board[x, y]));
+
+                }
             }
         }
     }
