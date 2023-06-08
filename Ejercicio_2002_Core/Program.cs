700b94357c4b351d3184555e66274daaeee7b889
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2023-06-07 20:20:48 -0600

    Ejercicio 2002

---
+++ Ejercicio_2002_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_2002_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
