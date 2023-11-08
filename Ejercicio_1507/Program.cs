a7112eb8f2b565a964f5156c7295e49ee8e8ea3f
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2023-11-07 18:16:52 -0600

    Ejercicio_1507

---
+++ Ejercicio_1507/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ejercicio_1507
+{
+    internal class Program
+    {
+        static bool isSubsequence(string origin, string seed, int init)
+        {
+            int index = origin.IndexOf(seed[0], init);
+            if (index == -1)
+                return false;
+            else
+                return seed.Length == 1 ? true : isSubsequence(origin, seed.Remove(0, 1), index+1);
+        }
+        static void Main(string[] args)
+        {
+            int tc, total;
+            string origin, seed;
+            tc = int.Parse(Console.ReadLine());
+            for(int i = 0; i < tc; ++i)
+            {
+                origin = Console.ReadLine();
+                total = int.Parse(Console.ReadLine());
+                for(int j = 0; j < total; ++j)
+                {
+                    seed = Console.ReadLine();
+                    if (seed.Length > origin.Length)
+                        Console.WriteLine("No");
+                    else if (seed.Length == origin.Length)
+                        Console.WriteLine(seed == origin ? "Yes" : "No");
+                    else
+                        Console.WriteLine(isSubsequence(origin, seed, 0) ? "Yes" : "No");
+                }
+            }
+        }
+    }
+}
