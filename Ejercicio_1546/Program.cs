488758a27f7532e6cd1b05399178557ea9c90738
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2023-11-10 17:31:04 -0600

    Ejercicio_1546

---
+++ Ejercicio_1546/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1546
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "", "Rolien", "Naej", "Elehcim", "Odranoel" };
+            string data;
+            int n;
+            while ((data = Console.ReadLine()) != null)
+            {
+                n = int.Parse(data);
+                for (int i = 0; i < n; ++i)
+                {
+                    Console.WriteLine(names[int.Parse(Console.ReadLine())]);
+                }
+            }
+        }
+    }
+}
