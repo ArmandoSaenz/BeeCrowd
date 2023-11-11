6b3fda5e78c867e69ab3f8d2cc6836e8e1f25f72
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2023-11-10 18:35:33 -0600

    Ejercicio_E_Code_Clash_2023

--- Ejercicio_E/Program.cs
+++ Ejercicio_E/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Ejercicio_E
 {
@@ -10,6 +12,29 @@ namespace Ejercicio_E
     {
         static void Main(string[] args)
         {
+            string data;
+            string[] values;
+            int N, K, mod, can1, can2;
+            int[] pos;
+            data = Console.ReadLine();
+            values = data.Split(' ');
+            N = int.Parse(values[0]);
+            K = int.Parse(values[1]);
+            pos = new int[K];
+            can1 = N / K;
+            can2 = can1 + 1;
+            mod = N % K;
+            List<string> Students = new List<string>();
+            while((data = Console.ReadLine()) != null)
+                Students.Add(data);
+            Students.Sort();
+            for(int i = 1; i < K; ++i)
+            {
+                if(i )
+                    pos[i] = 
+            }
+
+
         }
     }
 }
