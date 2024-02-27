7c631f174fd05b6837d259ecc41b79c25b0c840d
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2024-02-26 19:54:50 -0600

    Comentarios

--- Ejercicio_1016/Program.cs
+++ Ejercicio_1016/Program.cs
@@ -10,8 +10,11 @@ namespace Ejercicio_1016
     {
         static void Main(string[] args)
         {
+            //Se lee la velocidad
             int distance = int.Parse(Console.ReadLine());
+            //Se calcula el tiempo con una constante de 2 min/km
             int time = distance * 2;
+            //Se imprime resultado
             Console.WriteLine($"{time} minutos");
         }
     }
