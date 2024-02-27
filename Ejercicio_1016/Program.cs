a8ddd8bb2c760de0aec01e705120cc68bf89e5e0
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2024-02-26 19:52:58 -0600

    Ejercicio_1016

---
+++ Ejercicio_1016/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int distance = int.Parse(Console.ReadLine());
+            int time = distance * 2;
+            Console.WriteLine($"{time} minutos");
+        }
+    }
+}
