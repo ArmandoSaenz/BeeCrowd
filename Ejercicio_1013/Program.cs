1de7e1a24719f59e1089281bd058c439dbae121b
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2024-02-26 19:39:43 -0600

    Ejercicio_1013

---
+++ Ejercicio_1013/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se lee la linea
+            string data = Console.ReadLine();
+            //Se convierten los tres numeros a entero
+            List<int> values = Array.ConvertAll<string, int>(data.Split(' '), int.Parse).ToList();
+            //Se imprime el resultado
+            Console.WriteLine($"{values.Max()} eh o maior");
+        }
+    }
+}
+
