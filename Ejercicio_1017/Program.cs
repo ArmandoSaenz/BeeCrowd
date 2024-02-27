242bba9881218ae58515212528ebe0b7f70af177
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2024-02-26 19:58:20 -0600

    Ejercicio_1017

---
+++ Ejercicio_1017/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tiempo en horas
+            double time = double.Parse(Console.ReadLine());
+            //Velocidad
+            double vel = double.Parse(Console.ReadLine());
+            //Gasto de gasolita con una relación de 12 km / l
+            double gasoline = (time * vel) / 12;
+            //Impresión de resultado
+            Console.WriteLine(gasoline.ToString("F3"));
+        }
+    }
+}
