3e8d08efcda243d646da35225a052dfe3a055fb8
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2024-02-26 19:49:28 -0600

    Ejercicios 1014 y 1015

---
+++ Ejercicio_1014/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            float y = float.Parse(Console.ReadLine());
+            float z = x / y;
+            Console.WriteLine($"{z.ToString("F3")} km/l");
+        }
+    }
+}
---
+++ Ejercicio_1015/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            double[] v1, v2;
+            data = Console.ReadLine();
+            v1 = Array.ConvertAll<string, double>(data.Split(' '), double.Parse);
+            data = Console.ReadLine();
+            v2 = Array.ConvertAll<string, double>(data.Split(' '), double.Parse);
+            double output = Math.Sqrt(Math.Pow(v1[0] - v2[0], 2) + Math.Pow(v1[1] - v2[1], 2));
+            Console.WriteLine(output.ToString("F4"));
+        }
+    }
+}
