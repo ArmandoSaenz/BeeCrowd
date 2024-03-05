dfa55cac6f2a8c364c7ddd5bb7bf3f6b3608f1b2
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2024-03-04 18:37:51 -0600

    Ejercicio_1019

---
+++ Ejercicio_1019/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio_1019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int seconds;
+            TimeSpan time;
+            seconds = int.Parse(Console.ReadLine());
+            time = new TimeSpan(0, 0, seconds);
+            Console.WriteLine($"{time.Hours + time.Days*24}:{time.Minutes}:{time.Seconds}");
+            Console.ReadLine();
+        }
+    }
+}
