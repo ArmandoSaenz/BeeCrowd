73d22a9b3505bc7c02f21cd0d9ac8c0a6ae0a664
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2024-03-04 18:45:31 -0600

    Ejercicio_1020

---
+++ Ejercicio_1020/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> Period = new Dictionary<string, int>() { {"ano(s)", 365},{"mes(es)", 30},{"dia(s)", 1} };
+            int days = int.Parse(Console.ReadLine());
+            foreach(KeyValuePair<string, int> item in Period)
+            {
+                Console.WriteLine($"{days / item.Value} {item.Key}");
+                days = days % item.Value;
+            }
+            Console.ReadLine();
+        }
+    }
+}
