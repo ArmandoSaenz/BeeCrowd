4c6fb4072788c009787250b9775083aaa063ca52
José Armando Sáenz Esqueda <armando.saenz@diractum.com>
2024-03-08 18:08:29 -0600

    Ejercicio_1238

---
+++ Ejercicio_1238/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1238
+{
+    internal class Program
+    {
+        static void Combine(Queue<char> word1, Queue<char> word2, Queue<char> word3)
+        {
+            if (word1.Count > 0)
+                word3.Enqueue(word1.Dequeue());
+            if (word2.Count > 0)
+                word3.Enqueue(word2.Dequeue());
+            if (word1.Count == 0 && word2.Count == 0)
+                return;
+            else
+                Combine(word1, word2, word3);
+
+        }
+        static void Main(string[] args)
+        {
+            int totalcases = int.Parse(Console.ReadLine());
+            Queue<char> word1, word2, newword;
+            for(int ncase = 0; ncase < totalcases; ++ncase)
+            {
+                string data = Console.ReadLine();
+                string[] words = data.Split(' ');
+                word1 = new Queue<char>(words[0]);
+                word2 = new Queue<char>(words[1]);
+                newword = new Queue<char>();
+                Combine(word1, word2, newword);
+                Console.WriteLine(String.Concat(newword));
+            }
+        }
+    }
+}
