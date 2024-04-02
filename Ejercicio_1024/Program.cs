a40b7bff4d04eae37e3ef84eb5ac51c1611baaf6
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2024-04-02 11:40:54 -0600

    Ejercicio 1025

--- Ejercicio_1024/Program.cs
+++ Ejercicio_1024/Program.cs
@@ -8,8 +8,30 @@ namespace Ejercicio_1024
 {
     internal class Program
     {
+        static string Encriptar1(string data)
+        {
+            var palabra = data.ToList().Select(x => char.IsLetter(x) ? x + 3 : x);
+            return String.Concat(palabra.Select(x => (char)x));
+        }
+        static string Encriptar2(string data)
+        {
+            var palabra = data.ToList().Select(x => x - 1);
+            return String.Concat(palabra.Select(x => (char)x));
+        }
         static void Main(string[] args)
         {
+            int t = int.Parse(Console.ReadLine());
+            List<string> resultado = new List<string>();
+            for (int i = 0; i < t; ++i)
+            {
+                string data = Console.ReadLine();
+                int pos = data.Length / 2;
+                data = Encriptar1(String.Concat(data.Reverse()));
+                string subcadena = data.Substring(pos);
+                data = data.Replace(subcadena, Encriptar2(subcadena));
+                resultado.Add(data);
+            }
+            resultado.ForEach(x => Console.WriteLine(x));
         }
     }
 }
---
+++ Ejercicio_1025/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            string[] datos;
+            List<int> Canicas = new List<int>();
+            List<int> Preguntas = new List<int>();
+            string end = "0 0";
+            int n, q;
+            for(int i = 1; (data = Console.ReadLine())!= end; ++i)
+            {
+                datos = data.Split(' ');
+                n = int.Parse(datos[0]);
+                q = int.Parse(datos[1]);
+                for(int j = 0; j < n; ++j)
+                    Canicas.Add(int.Parse(Console.ReadLine()));
+                for (int j = 0; j < q; ++j)
+                    Preguntas.Add(int.Parse(Console.ReadLine()));
+                Canicas.Sort();
+                Console.WriteLine($"CASE# {i}:");
+                Preguntas.ForEach(x => Console.WriteLine((Canicas.Contains(x) ? $"{x} found at {Canicas.IndexOf(x) + 1}" : $"{x} nor found")));
+            }
+        }
+    }
+}
