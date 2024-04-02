0a986322cacd9edf20ac6081f4ce1a7c5b537af0
ArmandoSaenz <43352833+ArmandoSaenz@users.noreply.github.com>
2024-04-02 13:17:00 -0600

    Ejercicio_1025

--- Ejercicio_1025/Program.cs
+++ Ejercicio_1025/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,23 +13,28 @@ namespace Ejercicio_1025
         {
             string data;
             string[] datos;
-            List<int> Canicas = new List<int>();
-            List<int> Preguntas = new List<int>();
+            int[] Canicas;
+            int[] Preguntas;
+            int cantidad;
             string end = "0 0";
             int n, q;
-            for(int i = 1; (data = Console.ReadLine())!= end; ++i)
+            for (int i = 1; (data = Console.ReadLine()) != end; ++i)
             {
                 datos = data.Split(' ');
                 n = int.Parse(datos[0]);
                 q = int.Parse(datos[1]);
-                for(int j = 0; j < n; ++j)
-                    Canicas.Add(int.Parse(Console.ReadLine()));
+                Canicas = new int[10001];
+                Preguntas = new int[q];
+                for (int j = 0; j < n; ++j)
+                    ++Canicas[int.Parse(Console.ReadLine())];
                 for (int j = 0; j < q; ++j)
-                    Preguntas.Add(int.Parse(Console.ReadLine()));
-                Canicas.Sort();
+                    Preguntas[j] = int.Parse(Console.ReadLine());
                 Console.WriteLine($"CASE# {i}:");
-                Preguntas.ForEach(x => Console.WriteLine((Canicas.Contains(x) ? $"{x} found at {Canicas.IndexOf(x) + 1}" : $"{x} nor found")));
+                cantidad = 1;
+                var dictionary = Canicas.Select((value, index) => new { index, value }).ToDictionary(item => item.index, item => (Canicas[item.index] == 0 ? new int[] { 0, 0 } : new int[] { cantidad, (cantidad += Canicas[item.index]) }));
+                Preguntas.ToList().ForEach(x => Console.WriteLine((dictionary[x][0] != 0 ? $"{x} found at {dictionary[x][0]}" : $"{x} not found")));
             }
+            Console.ReadLine();
         }
     }
 }
