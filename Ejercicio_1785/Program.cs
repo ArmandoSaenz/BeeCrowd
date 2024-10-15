d807eec93232f8faff523e32323ebbfc86c54693
ArmandoSaenz <jase1588@gmail.com>
2024-10-14 20:55:49 -0600

    Ejercicios 1785 y 1872

---
+++ Ejercicio_1785/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ejercicio_1785
+{
+    internal class Program
+    {
+        static int[] invalids = new int[] {0, 1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999 };
+        static int krapekar(int X)
+        {
+            if(invalids.Contains(X))
+            { 
+                return -1; 
+            }
+            int cnt = 0;
+            int hi, lo;
+            while (X != 6174)
+            {
+                (lo, hi) = Combinacion(X);
+                X = hi - lo;
+                cnt = cnt + 1;
+            }
+            return cnt;
+        }
+
+        static (int, int) Combinacion(int X)
+        {
+            var digits = X.ToString().PadLeft(4,'0').Select(digit => int.Parse(digit.ToString())).ToArray();
+            return (digits.OrderBy(x => x).Aggregate((acc, d) => acc * 10 + d), digits.OrderByDescending(x => x).Aggregate((acc, d) => acc * 10 + d));
+        }
+
+        static void Main(string[] args)
+        {
+            int tc;
+            int num;
+            tc = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= tc; i++)
+            {
+                num = int.Parse(Console.ReadLine());
+                
+                Console.WriteLine($"Case {i}: {krapekar(num)}");
+            }    
+        }
+    }
+}
--- Ejercicio_1872/Program.cs
+++ Ejercicio_1872/Program.cs
@@ -1,42 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ejercicio_1872
 {
+    class Number
+    {
+        public string data;
+        public string Data
+        {
+            get
+            {
+                return data;
+            }
+            private set
+            {
+                data = value;
+                var parts = data.Split('.');
+                Value = decimal.Parse(data);
+                IntegerPart = parts[0];
+                DecimalPart = parts.Length > 1 ? parts[1] : "";
+                Integer = (int)(int.Parse(IntegerPart) * Math.Pow(10, NumbersOfDecimal - IntegerPart.Length));
+                try
+                {
+                    Decimal = (int)(int.Parse(DecimalPart) * Math.Pow(10, NumbersOfDecimal - DecimalPart.Length));
+                }
+                catch 
+                {
+                    Decimal = 0;
+                }
+            }
+
+        }
+        public decimal Value { get; set; }
+        public string ReverseString(string str) => string.Concat(str.Reverse());
+        public bool IsPalindrome(string str) => str == ReverseString(str);
+        public string IntegerPart { get; private set; }
+        public string DecimalPart { get; private set; }
+        public int Integer { get; private set; }
+        public int Decimal { get; private set; }
+        public bool IsDecimal => Data.Contains(".");
+        public int NumbersOfDecimal => Math.Max(IntegerPart.Length, DecimalPart.Length);
+        public Number(string _data)
+        {
+            Data = _data;
+        }
+
+        public string GetDifference()
+        {
+            if(IsPalindrome(Data))
+            {
+                return 0.ToString();
+            }
+            else if(!IsDecimal||Decimal==0)
+            {
+                return $"0.{ReverseString(IntegerPart)}";
+            }
+            int newvalue = (int)(int.Parse(ReverseString(IntegerPart)) * Math.Pow(10, NumbersOfDecimal - IntegerPart.Length));
+            if (newvalue>=Decimal)
+            {
+                return ((newvalue - Decimal)/Math.Pow(10d,NumbersOfDecimal)).ToString($"F{NumbersOfDecimal}");
+            }
+            else
+            {
+                if (IsPalindrome((++Integer).ToString()))
+                {
+                    return (Integer - Value).ToString();
+                }
+                else
+                {
+                    var difference1 = Integer - Value;
+                    var difference2 = (decimal)(int.Parse(ReverseString(Integer.ToString())) / Math.Pow(10d, NumbersOfDecimal));
+                    return (difference1+difference2).ToString();
+                }
+            }
+        }
+    }
     internal class Program
     {
-        static string ReverseString(string str) => string.Concat(str.Reverse());
-        static bool IsPalindrome(string str) => str == ReverseString(str);
-
         static void Main(string[] args)
         {
             int n;
             string data;
-            decimal entero, decimales;
-            decimal numero;
-            int dim1, dim2;
-            string[] partes;
-            decimal increment;
+            string result;
+            Number number;
             n = int.Parse(Console.ReadLine());
             while (!String.IsNullOrEmpty((data = Console.ReadLine())))
             {
-                if (IsPalindrome(data))
-                {
-                    Console.WriteLine("0");
-                    continue;
-                }
-                //Obteniendo el numero
-                numero = decimal.Parse(data);
-                //Separando enteros y decimales
-                partes = data.Split('.');
-                //Contando digitos de la parte entera
-                dim1 = partes[0].Length;
-                //Contando digitos de la parte decimal
-                dim2 =  partes.Length == 2 ? partes[1].Length : 0;
-                increment = (decimal)Math.Pow(10, -Math.Max(dim1, dim2));
-                for (; !IsPalindrome(numero.ToString()); numero += increment) ;
+                number = new Number(data);
+                for (result = number.GetDifference(); result != "0" && result.Last() == '0'; result = result.Remove(result.Length - 1, 1)) ;
+                Console.WriteLine(result);
             }
         }
     }
