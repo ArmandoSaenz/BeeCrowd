460bbcddac15243241f305ff97dea46e6dbb82b6
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-03-25 23:01:50 -0600

    Ejercicios_1170 y Ejercicio_2025

---
+++ Ejercicio_1170/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_1170
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] limfood = new int[] {0, 1, 2, 4, 8, 16, 32, 64, 128, 255, 512, 1024 };
+            int t = int.Parse(Console.ReadLine());
+            double food;
+            int day;
+            for(int i = 0; i <t; ++i)
+            {
+                food = double.Parse(Console.ReadLine());
+                for (day = 0; food > limfood[day]; ++day);
+                --day;
+                Console.WriteLine($"{day} dias");
+            }
+        }
+    }
+}
---
+++ Ejercicio_2025/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ejercicio_2025
+{
+    class Program
+    {
+        static string ReplaceWord(string word)
+        {
+            string tmp=word;
+            switch(word.Length)
+            {
+                case 11:
+                    if (word.EndsWith(".") && word.Substring(1, 8) == "oulupukk")
+                        tmp = "Joulupukki.";
+                    break;
+                case 10:
+                    if (word.Substring(1,8)=="oulupukk")
+                        tmp = "Joulupukki";
+                    break;
+                
+            }
+            return tmp;
+        }
+        static void Main(string[] args)
+        {
+            string line;
+            int n;
+            List<string> words;
+            n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; ++i)
+            {
+                line = Console.ReadLine();
+                words = line.Split(' ').ToList();
+                words = words.Select(word => word.Length==10 || word.Length==11 ? ReplaceWord(word) : word).ToList();
+                Console.WriteLine(String.Join(" ", words));
+            }
+        }
+    }
+}
