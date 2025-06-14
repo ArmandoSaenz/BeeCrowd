111a250cae4d9424c1fd301852a231d812e40eb7
José Armando Sáenz Esqueda <jase1588@gmail.com>
2025-06-13 21:35:04 -0600

    Ejercicio 1153 y Ejercicio 2615

---
+++ Ejercicio_1153/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Ejercicio_1153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 1;
+            for (int i = int.Parse(Console.ReadLine()); i > 1; result *= i--);
+            Console.WriteLine(result);
+
+        }
+    }
+}
